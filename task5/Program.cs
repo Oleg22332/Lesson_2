3d14557ce5d6ed1183535984ded8f56cbfcd3ddb
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2025-03-12 10:58:25 +0300

    add task5

---
+++ task5/Program.cs
@@ -0,0 +1,5 @@
+﻿//На вход натуральное число N, на выход его цифры через запятую.
+string N = Console.ReadLine();
+for (int i = 1; i < N.Length; i++)
+    Console.Write($"{N[i - 1]}, ");
+Console.Write($"{N[N.Length - 1]}");
