0253eb416f6bfbedd5d020eb0f53b65783e4cc2f
Oleg Suroegin <suroegin.oleg.07122004@gmail.com>
2025-03-12 08:29:09 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = new int [5];
+array[0] = 5;
+array[1] = 7;
+array[2] = 5;
+array[3] = 4;
+array[4] = 3;
+for(int i = 0; i < 5; i++)
+    Console.Write($"{array[i]} ");
+int[] array2 = new int [5] { 1, 2, 3, 4, 5 }
+int[] array3 = { 1, 2, 3, 4, 5 }
